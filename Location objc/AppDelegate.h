34ff31824cf078fe433ddf9013ae00c1c4908b9f
Marchenko Nikita <marchenkonikitos@gmail.com>
2018-04-16 15:38:51 +0300

    Initial Commit

---
+++ Location objc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Location objc
+//
+//  Created by Nikita Marchenko on 4/16/18.
+//  Copyright © 2018 Nikita Marchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Location objc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Location objc
+//
+//  Created by Nikita Marchenko on 4/16/18.
+//  Copyright © 2018 Nikita Marchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
